b41b5a347dddb284c3150c38a451963e3ca13c87
Anthony Odey <anthonyogar61@gmail.com>
2023-04-21 16:28:49 +0100

    added a header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+extern char **environ;
+
+#endif /* SHELL_H */
