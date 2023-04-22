1ceeef47e67e02c3f09bd6731541e90454332f6b
Anthony Odey <anthonyogar61@gmail.com>
2023-04-22 23:00:08 +0100

    Updated the header file

--- shell.h
+++ shell.h
@@ -13,6 +13,16 @@
 #include <errno.h>
 #include <stddef.h>
 
+#define BUFSIZE 1024
+
+/**
+ * struct data_of_program - data needed for the execution of the program
+ * @input_buffer: buffer to hold user input
+ * @commands: array of pointers to hold individual commands
+ * @env: pointer to environment
+ * @status: exit status
+ */
+
 #include "macros.h" /* for msg help and prompt */
 
 /* data of the program struct */
@@ -26,6 +36,7 @@ typedef struct data_of_program
 	int exit_status; /* exit status */
 } data_of_program;
 
+	/* shell.c */
 /* initialize the struct with the info of the program */
 void initialize_data(data_of_program *data, int argc, char *argv[], char **env);
 
@@ -34,6 +45,15 @@ void sisifo(char *prompt, data_of_program *data);
 
 /* print the prompt in a new line */
 void handle_ctrl_c(int opr UNUSED);
+
+
+
+	/* _getline.c */
+/* read one line of the standar input */
+int _getline(data_of_program *data);
+
+/* split the each line for the logical operators if it exist */
+int check_logic_ops(char *array_commands[], int i, char array_operators[]);
 
 
 
