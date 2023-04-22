16f87fe74b89e5755503c94bd1f7b7a6d8cd750c
Anthony Odey <anthonyogar61@gmail.com>
2023-04-22 15:24:03 +0100

    updated the header file

--- shell.h
+++ shell.h
@@ -4,12 +4,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.H>
+#include <errno.h>
+#include <stddef.h>
 
-#define BUFFER_SIZE 1024
+#include "macros.h" /* for msg help and prompt */
 
-extern char **environ;
-int execute_command(char **args);
+/* data of the program struct */
+typedef struct data_of_program
+{
+	char **environ; /* Enviroment variables */
+	char **args; /* Arguments */
+	char **commands; /* list of commands */
+	char *buffer; /* line read from standard input */
+	char *prompt; /* shell prompt */
+	int exit_status; /* exit status */
+} data_of_program;
+
+/* initialize the struct with the info of the program */
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env);
+
+/* makes the infinite loop that shows the prompt */
+void sisifo(char *prompt, data_of_program *data);
+
+/* print the prompt in a new line */
+void handle_ctrl_c(int opr UNUSED);
+
+
+
 
 #endif /* SHELL_H */
