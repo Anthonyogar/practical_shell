69c4907ec8f32210bad5332c1cb3d88012d40bb1
Anthony Odey <anthonyogar61@gmail.com>
2023-04-22 14:55:37 +0100

    Updated the lines in this file

--- 0-shell.c
+++ 0-shell.c
@@ -1,47 +1,117 @@
 #include "shell.h"
 
 /**
- * main - entry point
+ * main - initialize the variables of the program
+ * @argc: number of values recieved from the command line
+ * @argv: values recived from the command line
+ * @env: number of values recieved from the command line
  *
  * Return: 0 on success, non-zero on failure
  */
-int main(void)
+int main(int argc, char *argv[], char *env[])
 {
-	char buffer[BUFFER_SIZE];
+	/* declare the data structure and set the pointer */
+	data_of_program data_structure = {NULL}, *data = &data_struct;
+	/* declare an empty prompt string */
+	char *prompt = "";
+
+	/* initialize the data structure with program info */
+	initialize_data(data, argc, argv, env);
+
+	/* register signal handleer for SIGINT */
+	signal(SIGINT, handle_ctrl_c);
+
+	/*Check if we are in the interactive mode in a terminal */
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+	{
+		/* set errno to a custom value to indicate inteeractive mode */
+		errno = INTERACTIVE_ERRNO;
+		/* set the prompt to default prompt message */
+		prompt = DEFAULT_PROMPT;
+	}
+
+	/* reset errno to 0 */
+	errno = 0;
+
+	/* start the main loop */
+	sisifo(prompt. data);
+
+	/* Return 0 to indicate success */
+	return (0);
+}
+
+/**
+ * initialize_data - initialize the struct with the info
+ *                   of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments passed to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values recieved from the command line
+ */
+
+/* Initialixze the struct with the info of the program */
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+	data->environ = env;
+	data->args = argv;
+	data->commands = NULL;
+	data->buffer = NULL;
+	data->prompt = NULL;
+	data->exit_status = 0;
+}
+
+/**
+ * sisifo - its a infinte loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
+ */
+
+/* makes the infinite loop that shows the prompt */
+void sisifo(char *prompt, data_of_program *data)
+{
+	size_t len = 0;
+	ssize_t read = 0;
 
 	while (1)
 	{
-		printf("$ ");
+		if (isatty(STDIN_FILENO))
+			printf("%s", prompt);
 
-		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL )
+		read = getline(&(data->buffer), &len, stdin);
+
+		if (read == 1)
 		{
-			if (feof(stdin))
-			{
-				exit(0);
-			}
-			else
-			{
-				perror("fgets");
-				exit(1);
-			}
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			exit(0);
 		}
 
-		/* Remove newline character */
-		buffer[strcspnz(buffer, "\n")] = '\0';
+		/* remove trailing newline character */
+		data->buffer[read - 1] = '\0';
 
-		/* chacks if command is an executable file */
-		if (access(buffer, X_OK) == -1)
-		{
-			printf("%s: command not found\n", buffer);
+		/* handle empty input */
+		if (strcmp(data->buffer, "") == 0)
 			continue;
-		}
 
-		/* execute command */
-		if (execute_commmand(args) == -1)
-		{
-			printf("Failed to execute command\n");
-		}
+		/* handle ctrl-C signal */
+		signal(SIGINT, handle_ctrl_c);
+
+		/* parse input and execute command */
+		data->commands = parse_input(data->buffer);
+		execute_command(data);
 	}
+}
 
-	return (0);
+/**
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is sent to the program
+ * @UNUSED: option of prorotype
+ */
+
+/* print the prompt in a new line */
+void handle_ctrl_c(int opr UNUSED)
+{
+	printf("\n");
+	printf("%s", PROMPT);
+	fflush(stdout);
 }
