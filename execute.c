95909370212b938c52dcca92e8bbb9781f64538c
Anthony ogaiiiiir <anthonyogar61@gmail.com>
2023-05-03 16:14:00 +0100

    execute the commands parsed

--- execute.c
+++ execute.c
@@ -1,40 +1,49 @@
 #include "shell.h"
+
 /**
- * execute - execute a command with its entire path
- * @data: struct for the program's data
- *
- * Return: returns 0 on success, 1 on error
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the shell's data
+ * Return: If success returns zero, otherwise, return -1.
  */
-int execute(data_of_program *data)
+int execute(shell_data *data)
 {
-	struct stat file_stat;
-	int status;
+    int retval = 0, status;
+    pid_t pid;
 
-	if (data->command_namea == NULL)
-		return (-1);
+    /* check if program is a built-in command */
+    retval = check_builtins(data);
+    if (retval != -1) /* if program is a built-in command */
+        return (retval);
 
-	if (stat(data-command_name, &file_stat) == 0)
-	{
-		if ((file_stat.st_mode & S_IFMT) == S_IFDIR)
-			return (print_error(data, EISDIR));
-		if (access(data->command_name, X_OK) == 0)
-			return (execute_command(data));
-		else
-			return (print_error(data, EACCES));
-	}
-
-	if (find_path(data) == NULL)
-		return (print_error(data, ENOENT));
-
-	if (access(data->comman_path, X_OK) == 0)
-	{
-		status = execute_command(data);
-		free(data->command_path);
-		return (status);
-	}
-	else
-	{
-		free(data->command_path);
-		return (print_error(data, EACCES));
-	}
+    /* check if program exists in file system */
+    retval = find_program(data);
+    if (retval) /* if program not found */
+        return (retval);
+    else /* if program found */
+    {
+        pid = fork(); /* create a child process */
+        if (pid == -1)
+        { /* if the fork call failed */
+            perror(data->tokens[0]);
+            exit(EXIT_FAILURE);
+        }
+        if (pid == 0)
+        { /* I am the child process, I execute the program*/
+            retval = execve(data->program_path, data->tokens, data->env);
+            if (retval == -1) /* if error when execve*/
+            {
+                perror(data->tokens[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        { /* I am the parent process, I wait and check the exit status of the child */
+            wait(&status);
+            if (WIFEXITED(status))
+                errno = WEXITSTATUS(status);
+            else if (WIFSIGNALED(status))
+                errno = 128 + WTERMSIG(status);
+        }
+    }
+    return (0);
 }
